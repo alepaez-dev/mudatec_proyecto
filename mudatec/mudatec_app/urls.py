a9d264275317c4b0e69218094642565f91f40c3b
= <=>
2021-03-29 13:07:02 -0700

    Mudanza registro listo

--- mudatec/mudatec_app/urls.py
+++ mudatec/mudatec_app/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from .views import (
-  Home, UsuarioRegistro, UsuarioIniciarSesion
+  Home, 
+  UsuarioRegistro, 
+  UsuarioIniciarSesion,
+  MudanzaRegistro,
+  MudanzaIniciarSesion,
 )
 
 urlpatterns = [
     path("", Home),
     path("usuario/registro", UsuarioRegistro),
     path("usuario/iniciar_sesion", UsuarioIniciarSesion),
+    path("mudanza/registro", MudanzaRegistro),
+    path("mudanza/iniciar_sesion", MudanzaIniciarSesion),
 ]
--- mudatec/mudatec_app/views.py
+++ mudatec/mudatec_app/views.py
@@ -10,14 +10,25 @@ def Home(request):
     return HttpResponse(template.render(context,request))
 
 def UsuarioRegistro(request):
-    """Landing Page."""
+    """Usuario Registro"""
     context = {}
     template = loader.get_template("mudatec_app/usuario/registro.html")
     return HttpResponse(template.render(context,request))
 
 def UsuarioIniciarSesion(request):
-    """Landing Page."""
+    """Usuario Iniciar Sesion."""
     context = {}
     template = loader.get_template("mudatec_app/usuario/iniciar_sesion.html")
     return HttpResponse(template.render(context,request))
 
+def MudanzaRegistro(request):
+    """Mudanza Registro."""
+    context = {}
+    template = loader.get_template("mudatec_app/mudanza/registro.html")
+    return HttpResponse(template.render(context,request))
+
+def MudanzaIniciarSesion(request):
+    """Mudanza Iniciar Sesion."""
+    context = {}
+    template = loader.get_template("mudatec_app/mudanza/iniciar_sesion.html")
+    return HttpResponse(template.render(context,request))
